eba4fc5671fce1795e2b25b84f92de159157822f
David Zhao <davidzwb@gmail.com>
2020-11-02 02:52:55 +0800

    Add page after submission

--- index.js
+++ index.js
@@ -3,8 +3,20 @@ const app = express();
 const ejs = require("ejs");
 const port = 3000;
 
+app.use(express.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
+    let data = ejs.renderFile("./view/home.html");
+    data.then((data) => {
+        res.write(data);
+        res.end();
+    }).catch(() => {
+        console.log("error in rendering home page");
+    });
+});
+
+app.post("/todo", (req, res) => {
+    console.log(req.body);
     let data = ejs.renderFile("./view/home.html");
     data.then((data) => {
         res.write(data);
