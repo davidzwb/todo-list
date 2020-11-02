045245d747c985bc9c6d902e9f961a8c2d07c8a2
David Zhao <davidzwb@gmail.com>
2020-11-02 16:34:49 +0800

    Using env file for db connection

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 const express = require("express");
 const app = express();
+require('dotenv').config();
 const cookieParser = require("cookie-parser");
 const routes = require('./controller/routes');
 const port = 3000;
 
 const mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/todolist', { useNewUrlParser: true }, () => {
+mongoose.connect(`mongodb://${process.env.DB_HOST}/${process.env.DB_NAME}`, { useNewUrlParser: true }, () => {
     console.log("connected to db");
     app.listen(port, () => {
         console.log(`todo list app listening at http://localhost:${port}`)
