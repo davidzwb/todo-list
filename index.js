5c8fc4e5f74d867b08904968d52736928bca6ebf
David Zhao <davidzwb@gmail.com>
2020-11-02 10:35:54 +0800

    Refactor duplicate code

--- index.js
+++ index.js
@@ -3,13 +3,25 @@ const app = express();
 const ejs = require("ejs");
 const cookieParser = require("cookie-parser");
 const dataStore = require("./data-store");
-const { json } = require("express");
 
 let userID = 1;
 const port = 3000;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
+
+
+let renderHomePage = function (req, res) {
+    entries = dataStore.getEntries(req.cookies.todoID);
+
+    let data = ejs.renderFile("./view/home.html", { entries: entries });
+    data.then((data) => {
+        res.write(data);
+        res.end();
+    }).catch((error) => {
+        console.log("error in rendering home page: " + error);
+    });
+};
 
 app.get("/", (req, res) => {
     let entries = null;
@@ -32,30 +44,19 @@ app.get("/", (req, res) => {
         }
     }
 
-    let data = ejs.renderFile("./view/home.html", {entries:entries});
-    data.then((data) => {
-        res.write(data);
-        res.end();
-    }).catch((error) => {
-        console.log("error in rendering home page: " + error);
-    });
+    renderHomePage(req, res);
 });
 
 app.post("/todo", (req, res) => {
     console.log(req.body);
 
     dataStore.addEntry(req.cookies.todoID, req.body["plan"]);
-    entries = dataStore.getEntries(req.cookies.todoID);
 
-    let data = ejs.renderFile("./view/home.html", {entries:entries});
-    data.then((data) => {
-        res.write(data);
-        res.end();
-    }).catch((error) => {
-        console.log("error in rendering home page: " + error);
-    });
+    renderHomePage(req, res);
 });
 
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`)
 });
+
+
